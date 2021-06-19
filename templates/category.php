c00faa8af879793c116de648e2c992e8af46b233
JochenJung <jochen.jung@saitow.ag>
2021-06-19 07:56:06 +0200

    Fixed margins. Added link on title

--- templates/category.php
+++ templates/category.php
@@ -3,15 +3,15 @@
         <a href="index.html#arbeiten" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Zurück</a>
     </div>
 
-    <h2 class="mt-4 dancing-script"><?php echo htmlentities($title); ?></h2>
-    <div>
+    <h2 class="my-4 dancing-script"><?php echo htmlentities($title); ?></h2>
+    <div class="my-4">
         <?php echo htmlentities($description); ?>
     </div>
 
-    <div class="row mt-4">
+    <div class="row">
 <?php foreach ($items as $id => $item) { ?>
 <?php if ($id > 0 and $id % 4 === 0) { ?>    </div><div class="row mt-4"><?php } ?>
-        <div class="col">
+        <div class="col mb-3">
             <div class="card" style="width: 18rem;">
                 <img class="d-block mx-auto mb-4" src="img_256/<?php echo $item['src']; ?>" width="256" alt="<?php echo $item['name']; ?>"/>
                 <div class="card-body">
